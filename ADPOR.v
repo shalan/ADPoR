e9783b688149aed453e4d0b2e95d0f6a7014ebe8
Mohamed Shalan <mshalan@aucegypt.edu>
2020-12-03 16:03:03 +0200

    Update ADPOR.v

--- ADPOR.v
+++ ADPOR.v
@@ -1,9 +1,12 @@
 /*
     All digital PoR
+    Mohamed Shalan (mshalan@aucegypt.edu)
 */
+
 `timescale 1ns/1ps
 `default_nettype none
 
+// Enable the following only to simulate
 `define VERIFY
 
 module shift #(parameter LENGTH=16)
@@ -38,33 +41,6 @@ module ADPOR #(parameter LENGTH=16)
 
 endmodule
 
-/*
-module ADPOR #(parameter LENGTH=16)
-(
-    input clk,
-    output rst_n
-);
-
-    reg [LENGTH-1:0] chain1;
-    reg [LENGTH-1:0] chain2;
-    reg [LENGTH-1:0] chain3;
-    reg [LENGTH-1:0] chain4;
-    
-
-    always @(posedge clk) begin
-        chain1 <= {1'b1, chain1[LENGTH-1:1]};
-        chain2 <= {1'b0, chain2[LENGTH-1:1]};
-        chain3 <= {1'b1, chain3[LENGTH-1:1]};
-        chain4 <= {1'b0, chain4[LENGTH-1:1]};
-    end
-
-    assign rst_n =  (chain1=={LENGTH{1'b1}}) &
-                    (chain2=={LENGTH{1'b0}}) &
-                    (chain3=={LENGTH{1'b1}}) & 
-                    (chain4=={LENGTH{1'b0}});
-
-endmodule
-*/
 
 `ifdef VERIFY
 module ADPOR_tb;
@@ -79,6 +55,7 @@ module ADPOR_tb;
         $dumpfile("adpor.vcd");
         $dumpvars;
         clk = 0;
+        // load shift registers with random data to simulate the power on behavior 
         force MUV.reg1.shift_reg = $urandom & 12'hFFF;
         force MUV.reg2.shift_reg = $urandom & 12'hFFF;
         force MUV.reg3.shift_reg = $urandom & 12'hFFF;
